a06b2cb638aa25948dd04a885c357e64a9bb8d05
danishkodemonkey <humberjini@hotmail.com>
2024-11-27 14:41:52 +0100

    Styling pass for Ability stats component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 /// <reference path="./images.d.ts" />
 import React from "react";
 import "./App.css";
+import Ability from "./components/character/Ability.tsx";
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="bg-red-500">poop</h1>
-      <h1>Hello world</h1>
-      <h2>Welcome</h2>
-      <h3>H3</h3>
-      <h4>H4</h4>
-      <h5>H5</h5>
-      <h6>H6</h6>
-      <p>Paragraph</p>
-      <a href="">Link</a>
-      <span>Span</span>
-      <div></div>u
-    </div>
+    <>
+      <Ability />
+    </>
   );
 }
 
