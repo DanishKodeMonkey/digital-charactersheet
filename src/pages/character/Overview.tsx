d2831bef4db340161f1d15c1a8337566036a17c6
danishkodemonkey <humberjini@hotmail.com>
2024-11-27 17:35:29 +0100

    Layout pass for medium breakpoint

--- src/pages/character/Overview.tsx
+++ src/pages/character/Overview.tsx
@@ -2,19 +2,20 @@
 import React from "react";
 import Ability from "../../components/character/Ability.tsx";
 import CharacterInformation from "../../components/character/CharacterInformation.tsx";
+import Status from "../../components/character/Status.tsx";
 
 
 function Overview() {
   return (
-        <div className="container min-h-screen mx-auto grid grid-cols-1 md:grid-cols-3">
-            <div className="col-span-full md:col-span-3 bg-red-300"><CharacterInformation /></div>
-            <div className="col-span-full md:col-span-3 bg-green-300">HEALTH AND AC</div>
-            <div className="col-span-full md:col-span-1 bg-blue-300"><Ability /></div>
-            <div className="col-span-full md:col-span-1 md:col-start-2 bg-pink-300">INITIATIVE AND <br />  BASE ATTACK BONUS</div>
-            <div className="col-span-full md:col-span-2 bg-yellow-300">SAVING THROWS</div>
-            <div className="col-span-full md:col-span-2 bg-amber-500">WEAPONS AND ARMOR <div>WEP 1</div> <div>WEP 2</div> <div>WEP 3</div> <div>ARMOR</div> <div>SHIELD</div> <div>Misc</div></div>
+        <div className="container min-h-screen lg:mx-auto max-xl:min-w-full grid grid-cols-1 md:grid-cols-3">
+            <div className="col-span-full lg:col-span-3 bg-red-300"><CharacterInformation /></div>
+            <div className="col-span-full lg:col-span-3 bg-green-300"><Status /></div>
+            <div className="col-span-full md:col-span-2 md:row-span-2 lg:col-span-1 bg-blue-300"><Ability /></div>
+            <div className="col-span-full md:row-start-3 md:col-span-1 md:col-start-3 lg:col-start-2 bg-pink-300">INITIATIVE AND <br />  BASE ATTACK BONUS</div>
+            <div className="col-span-full md:row-start-4 md:col-span-1 md:col-start-3 lg:col-start-2 bg-yellow-300">SAVING THROWS</div>
+            <div className="col-span-full lg:col-span-2 bg-amber-500">WEAPONS AND ARMOR <div>WEP 1</div> <div>WEP 2</div> <div>WEP 3</div> <div>ARMOR</div> <div>SHIELD</div> <div>Misc</div></div>
 
-            <div className="md:col-start-3 md:row-start-3 md:row-span-3 bg-purple-300">SKILLS</div>
+            <div className="col-span-full lg:col-start-3 lg:row-start-3 lg:row-span-3 bg-purple-300">SKILLS</div>
         </div>
   );
 }
