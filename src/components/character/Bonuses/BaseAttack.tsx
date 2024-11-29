2bb4d72391b578e774d95fc7c4a2bd9746c89351
danishkodemonkey <humberjini@hotmail.com>
2024-11-29 13:01:43 +0100

    Initiative sub-component pass

---
+++ src/components/character/Bonuses/BaseAttack.tsx
@@ -0,0 +1,13 @@
+
+
+function BaseAttackBonus(){
+
+    return(
+        <div>
+        <h1>Base attack bonus sub-component</h1>
+        </div>
+    )
+}
+
+
+export default BaseAttackBonus
---
+++ src/components/character/Bonuses/Bonuses.tsx
@@ -0,0 +1,16 @@
+import BaseAttack from "./BaseAttack.tsx";
+import Initiative from "./Initiative.tsx";
+
+
+function Bonuses(){
+
+    return(
+        <div className="flex flex-col">
+        <BaseAttack />
+        <Initiative />
+        </div>
+    )
+}
+
+
+export default Bonuses
