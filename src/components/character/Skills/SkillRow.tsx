a333d37e5e162444d9629559d5e4a711a0f98bd1
danishkodemonkey <humberjini@hotmail.com>
2024-12-03 14:12:22 +0100

    deno fmt

--- src/components/character/Skills/SkillRow.tsx
+++ src/components/character/Skills/SkillRow.tsx
@@ -1,14 +1,13 @@
-import { useState, useEffect } from "react";
-
+import { useEffect, useState } from "react";
 
 // huskat: seperate types to typefile
 interface Skill {
-    name: string;
-    keyAbility: string;
+  name: string;
+  keyAbility: string;
 }
 
-interface SkillRowProps{
-    skill: Skill;
+interface SkillRowProps {
+  skill: Skill;
 }
 
 function SkillRow({ skill }: SkillRowProps) {
@@ -18,46 +17,77 @@ function SkillRow({ skill }: SkillRowProps) {
   const [ranks, setRanks] = useState<number>("");
   const [miscMod, setMiscMod] = useState<number>("");
 
-
-  const toggleLearned = () =>{
-    setLearned((prevLearned) => !prevLearned)
-  }
-useEffect(() => {
-  setSkillMod((abilityMod ? abilityMod : 0)+(ranks? ranks : 0) + (miscMod ? miscMod : 0))
-}, [abilityMod, ranks, miscMod]);
+  const toggleLearned = () => {
+    setLearned((prevLearned) => !prevLearned);
+  };
+  useEffect(() => {
+    setSkillMod(
+      (abilityMod ? abilityMod : 0) + (ranks ? ranks : 0) +
+        (miscMod ? miscMod : 0),
+    );
+  }, [abilityMod, ranks, miscMod]);
 
   // HUSKAT: Styling to sheet
   return (
     <div className="skill-row flex space-between">
       <div className="w-1/12">
-<input type="checkbox" name="learned" id="learned" checked={learned} onChange={toggleLearned}/></div>
+        <input
+          type="checkbox"
+          name="learned"
+          id="learned"
+          checked={learned}
+          onChange={toggleLearned}
+        />
+      </div>
       <label className="w-1/3 text-sm" htmlFor={`${skill.name}`}>
         {skill.name}
       </label>
       <span className="text-xs mr-2">{skill.keyAbility}</span>
       <div className="flex w-1/2">
-      <div className="w-1/4">
+        <div className="w-1/4">
           <input
             type="number"
             name="skillModifier"
             className="input-micro"
             id={`skillModifier-${skill.name}`}
             value={skillMod ? skillMod : 0}
-           disabled
+            disabled
           />
-      </div>
+        </div>
 
-          <div className="w-1/4">
-          <input className="input-micro" type="number" name="abilityModifier" id={`abilityModifier-${skill.name}`} value={abilityMod}    disabled    /></div>
-          <div className="w-1/4">
-          <input className="input-micro" type="number" name="ranksModifier" id={`ranksModifier-${skill.name}`} value={ranks}        onChange={(e) => setRanks(Number(e.target.value))}/></div>
-          <div className="w-1/4">
-          <input className="input-micro" type="number" name="miscModifier" id={`miscModifier-${skill.name}`} value={miscMod}        onChange={(e) => setMiscMod(Number(e.target.value))}/></div>
-          
-              </div>
+        <div className="w-1/4">
+          <input
+            className="input-micro"
+            type="number"
+            name="abilityModifier"
+            id={`abilityModifier-${skill.name}`}
+            value={abilityMod}
+            disabled
+          />
+        </div>
+        <div className="w-1/4">
+          <input
+            className="input-micro"
+            type="number"
+            name="ranksModifier"
+            id={`ranksModifier-${skill.name}`}
+            value={ranks}
+            onChange={(e) => setRanks(Number(e.target.value))}
+          />
+        </div>
+        <div className="w-1/4">
+          <input
+            className="input-micro"
+            type="number"
+            name="miscModifier"
+            id={`miscModifier-${skill.name}`}
+            value={miscMod}
+            onChange={(e) => setMiscMod(Number(e.target.value))}
+          />
+        </div>
       </div>
+    </div>
   );
 }
 
-
-export default SkillRow
+export default SkillRow;
--- src/components/character/Skills/Skills.tsx
+++ src/components/character/Skills/Skills.tsx
@@ -1,51 +1,70 @@
-import {useState} from 'react'
+import { useState } from "react";
 import SkillRow from "./SkillRow.tsx";
-import skillList from './skillList.json'
+import skillList from "./skillList.json";
 
 function Skills() {
-
-    const [maxRanks, setMaxRanks] = useState<number>(0)
-    const [skillPoints, setSkillPoints] = useState<number>(0)
-
+  const [maxRanks, setMaxRanks] = useState<number>(0);
+  const [skillPoints, setSkillPoints] = useState<number>(0);
 
   return (
     <div className="mx-2 my-2">
-
-
       <div className="flex flex-col">
-      <div className="flex gap-7">
+        <div className="flex gap-7">
           <div>
-              <h1 className="header-title">Skills</h1>
+            <h1 className="header-title">Skills</h1>
           </div>
           <div className="flex justify-around">
-              <div>
-                <label className="header-text text-[.6rem]" htmlFor="skillPoints">Max ranks / Skill Points</label></div>
-              <div className="flex w-2/6">
-                <input className="input-micro" type="number" name="maxRanks" id="maxRanks"  value={maxRanks} onChange={(e)=> setMaxRanks(e.target.value)}/>
-                <input className="input-micro" type="number" name="skillPoints" id="skillPoints"  value={skillPoints} onChange={(e)=> setSkillPoints(e.target.value)}/>
-              </div>
+            <div>
+              <label className="header-text text-[.6rem]" htmlFor="skillPoints">
+                Max ranks / Skill Points
+              </label>
+            </div>
+            <div className="flex w-2/6">
+              <input
+                className="input-micro"
+                type="number"
+                name="maxRanks"
+                id="maxRanks"
+                value={maxRanks}
+                onChange={(e) => setMaxRanks(e.target.value)}
+              />
+              <input
+                className="input-micro"
+                type="number"
+                name="skillPoints"
+                id="skillPoints"
+                value={skillPoints}
+                onChange={(e) => setSkillPoints(e.target.value)}
+              />
+            </div>
           </div>
-      </div>
-
-        <div className="flex">
-            <span className="text-[.5rem] sideways -ml-3">Learned</span>
-            <span className="header-text mr-14">Skill name</span>
-            <span className="header-text mr-1">Key <br /> ability</span>
-            <span className="header-text mr-2">Skill <br /> modifier</span>
-            <span className="header-text mr-2">Ability <br /> modifier</span>
-            <span className="header-text mr-3">Ranks</span>
-            <span className="header-text">misc <br /> modifier</span>
-            
         </div>
 
+        <div className="flex">
+          <span className="text-[.5rem] sideways -ml-3">Learned</span>
+          <span className="header-text mr-14">Skill name</span>
+          <span className="header-text mr-1">
+            Key <br /> ability
+          </span>
+          <span className="header-text mr-2">
+            Skill <br /> modifier
+          </span>
+          <span className="header-text mr-2">
+            Ability <br /> modifier
+          </span>
+          <span className="header-text mr-3">Ranks</span>
+          <span className="header-text">
+            misc <br /> modifier
+          </span>
+        </div>
       </div>
       <div className="skills-list flex flex-col">
-        <hr className="my-2 border-y-2"/>
-      <div className=" h-screen overflow-scroll">
+        <hr className="my-2 border-y-2" />
+        <div className=" h-screen overflow-scroll">
           {skillList.map((skill, index) => (
-              <SkillRow key={index} skill={skill} />
-            ))}
-      </div>
+            <SkillRow key={index} skill={skill} />
+          ))}
+        </div>
       </div>
     </div>
   );
