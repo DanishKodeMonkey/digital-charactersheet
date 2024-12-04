4ed1ad2b24a48dfe5568d02bbf0b1e1b9e7b3bc4
danishkodemonkey <humberjini@hotmail.com>
2024-12-04 15:40:15 +0100

    fix collapsable menu height adjustment

--- src/components/character/Gear/Gear.tsx
+++ src/components/character/Gear/Gear.tsx
@@ -13,60 +13,91 @@ function Gear() {
 
   return (
     <div className="px-2 py-2 space-y-4">
-      <div className="gear-container z-0">
-        <h1>GEAR</h1>
+    <div className="gear-container">
+      <h1>GEAR</h1>
 
-        <div className="p-4">
-          <div
-            className="weapons-container z-30"
+      <div className="p-4 space-y-4">
+        {/* Weapons Section */}
+        <div>
+          <h1
+            className="cursor-pointer"
             onClick={() => setIsWeaponsVisible(!isWeaponsVisible)}
           >
-            <h1>Weapons {isWeaponsVisible ? "v" : "^"}</h1>
-            {isWeaponsVisible && (
-              <div className="p-4">
-                <Weapon name={"Excalibur"} />
-                <Weapon name={"Frostmourne"} />
-                <Weapon name={"Battleaxe"} />
-              </div>
-            )}
-          </div>
+            Weapons {isWeaponsVisible ? "v" : "^"}
+          </h1>
           <div
-            className="armor-container"
+            className={` transition-max-height duration-500 ease-in-out overflow-hidden ${
+              isWeaponsVisible ? "max-h-[1000px]" : "max-h-0"
+            }`}
+          >
+            <div className="p-4">
+              <Weapon name={"Excalibur"} />
+              <Weapon name={"Frostmourne"} />
+              <Weapon name={"Battleaxe"} />
+            </div>
+          </div>
+        </div>
+
+        {/* Armor Section */}
+        <div>
+          <h1
+            className="cursor-pointer"
             onClick={() => setIsArmorVisible(!isArmorVisible)}
           >
-            <h1>Armor {isArmorVisible ? "v" : "^"}</h1>
-            {isArmorVisible && (
-              <div className="p-4">
-                <Armor name={"Heavy leather armor"} />
-              </div>
-            )}
-          </div>
+            Armor {isArmorVisible ? "v" : "^"}
+          </h1>
           <div
-            className="shield-container"
+            className={`transition-max-height duration-300 ease-in-out overflow-hidden ${
+              isArmorVisible ? "max-h-[1000px]" : "max-h-0"
+            }`}
+          >
+            <div className="p-4">
+              <Armor name={"Heavy leather armor"} />
+            </div>
+          </div>
+        </div>
+
+        {/* Shield Section */}
+        <div>
+          <h1
+            className="cursor-pointer"
             onClick={() => setIsShieldVisible(!isShieldVisible)}
           >
-            <h1>Shield {isShieldVisible ? "v" : "^"}</h1>
-            {isShieldVisible && (
-              <div className="p-4">
-                <Shield name={"Buckler"} />
-              </div>
-            )}
-          </div>
+            Shield {isShieldVisible ? "v" : "^"}
+          </h1>
           <div
-            className="shield-container"
+            className={`transition-max-height duration-300 ease-in-out overflow-hidden ${
+              isShieldVisible ? "max-h-[1000px]" : "max-h-0"
+            }`}
+          >
+            <div className="p-4">
+              <Shield name={"Buckler"} />
+            </div>
+          </div>
+        </div>
+
+        {/* Miscellaneous Items Section */}
+        <div>
+          <h1
+            className="cursor-pointer"
             onClick={() => setIsMiscVisible(!isMiscVisible)}
           >
-            <h1>Miscellaneous items {isMiscVisible ? "v" : "^"}</h1>
-            {isMiscVisible && (
-              <div className="p-4">
-                <MiscItem name={"Necklace of charming personality"} />
-                <MiscItem name={"The one ring"} />
-              </div>
-            )}
+            Miscellaneous items {isMiscVisible ? "v" : "^"}
+          </h1>
+          <div
+            className={`transition-max-height duration-300 ease-in-out overflow-hidden ${
+              isMiscVisible ? "max-h-[1000px]" : "max-h-0"
+            }`}
+          >
+            <div className="p-4">
+              <MiscItem name={"Necklace of charming personality"} />
+              <MiscItem name={"The one ring"} />
+            </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
   );
 }
 
