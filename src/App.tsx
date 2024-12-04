e447bf52d41aab27b081b3515f7e5a43834c7ba1
danishkodemonkey <humberjini@hotmail.com>
2024-12-04 15:56:31 +0100

    implement react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 /// <reference path="./images.d.ts" />
 import React from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import "./App.css";
+import Header from "./components/Header.tsx";
+
 import Overview from "./pages/character/Overview.tsx";
+import Character from "./pages/character/Character.tsx";
+import Inventory from "./pages/character/Inventory.tsx";
+import Spells from "./pages/character/Spells.tsx";
+
 
 function App() {
   return (
     <div className="App">
-      <Overview />
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Overview />} />
+            <Route path="/inventory" element={<Inventory />} />
+            <Route path="/character" element={<Character />} />
+            <Route path="/spells" element={<Spells />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
