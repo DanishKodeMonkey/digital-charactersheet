f8e9b9157ed225a7fc465547407ae6b1bae72d29
danishkodemonkey <humberjini@hotmail.com>
2024-12-04 15:24:05 +0100

    placeholder test for weapons

--- src/components/character/Gear/Weapon.tsx
+++ src/components/character/Gear/Weapon.tsx
@@ -18,116 +18,122 @@ function Weapon({ name }: WeaponTypeProp) {
   const [wepAmmoMax, setWepAmmoMax] = useState<number>("");
 
   return (
-    <div className="grid grid-cols-5 grid-rows-3 bg-red-300 bg-opacity-80 ring-2">
-      <div className="col-span-1 flex flex-col">
-        <label htmlFor="weapon-name">ATTACK</label>
-        <div>
-          <input
-            className="input-small"
-            type="text"
-            name="weapon-name"
-            id={`weapon-id-${name}`}
-            value={wepName}
-          />
-        </div>
+    <div className="grid grid-cols-8 gap-4 bg-red-300 bg-opacity-80 ring-2 p-2 m-2">
+      <div className="col-span-3 flex flex-col justify-between">
+        <label className="input-label" htmlFor="weapon-name">ATTACK</label>
+
+        <input
+          className="small-input input-string-text "
+          type="text"
+          name="weapon-name"
+          id={`weapon-id-${name}`}
+          value={wepName}
+          onChange={(e) => setWepName(e.target.value)}
+        />
       </div>
-      <div className="col-span-1 flex flex-col">
-        <label htmlFor="attack-bonus">ATTACK BONUS</label>
-        <div>
-          <input
-            className="input-small"
-            type="text"
-            name="attack-bonus"
-            id={`weapon-atkBonus-${name}`}
-            value={atkBonus}
-          />
-        </div>
+      <div className="col-span-2 flex flex-col justify-between">
+        <label className="input-label" htmlFor="attack-bonus">
+          ATTACK BONUS
+        </label>
+
+        <input
+          className="small-input input-string-text "
+          type="text"
+          name="attack-bonus"
+          id={`weapon-atkBonus-${name}`}
+          value={atkBonus}
+          onChange={(e) => setAtkBonus(e.target.value)}
+        />
       </div>
-      <div className="col-span-1 flex flex-col">
-        <label htmlFor="weapon-damage">DAMAGE</label>
-        <div>
-          <input
-            className="input-small"
-            type="text"
-            name="weapon-damage"
-            id={`weapon-damage-${name}`}
-            value={wepDamage}
-          />
-        </div>
+      <div className="col-span-2 flex flex-col justify-between">
+        <label className="input-label" htmlFor="weapon-damage">DAMAGE</label>
+
+        <input
+          className="small-input input-string-text "
+          type="text"
+          name="weapon-damage"
+          id={`weapon-damage-${name}`}
+          value={wepDamage}
+          onChange={(e) => setWepDamage(e.target.value)}
+        />
       </div>
-      <div className="col-span-1 flex flex-col">
-        <label htmlFor="weapon-crit">CRIT</label>
-        <div>
-          <input
-            className="input-small"
-            type="text"
-            name="weapon-crit"
-            id={`weapon-crit-${name}`}
-            value={wepCrit}
-          />
-        </div>
+      <div className="flex flex-col justify-between">
+        <label className="input-label" htmlFor="weapon-crit">CRIT</label>
+
+        <input
+          className="small-input input-string-text "
+          type="text"
+          name="weapon-crit"
+          id={`weapon-crit-${name}`}
+          value={wepCrit}
+          onChange={(e) => setWepCrit(e.target.value)}
+        />
       </div>
 
-      <div className="row-start-2 col-span-1 flex flex-col">
-        <label htmlFor="weapon-range">RANGE</label>
-        <div>
-          <input
-            type="text"
-            name="weapon-range"
-            id={`weapon-range-${name}`}
-            value={wepRange}
-          />
-        </div>
+      <div className="flex flex-col justify-between">
+        <label className="input-label" htmlFor="weapon-range">RANGE</label>
+
+        <input
+          type="text"
+          className="small-input input-string-text "
+          name="weapon-range"
+          id={`weapon-range-${name}`}
+          value={wepRange}
+          onChange={(e) => setWepRange(e.target.value)}
+        />
       </div>
-      <div className="row-start-2 col-span-1 flex flex-col">
-        <label htmlFor="weapon-type">TYPE</label>
-        <div>
-          <input
-            className="input-small"
-            type="text"
-            name="weapon-type"
-            id={`weapon-type-${name}`}
-            value={wepType}
-          />
-        </div>
+      <div className="flex flex-col justify-between">
+        <label className="input-label" htmlFor="weapon-type">TYPE</label>
+
+        <input
+          className="small-input input-string-text "
+          type="text"
+          name="weapon-type"
+          id={`weapon-type-${name}`}
+          value={wepType}
+          onChange={(e) => setWepType(e.target.value)}
+        />
       </div>
-      <div className=" row-start-2 col-span-3 flex flex-col">
-        <label htmlFor="weapon-notes">NOTES</label>
-        <div>
-          <input
-            className="input-small w-full"
-            type="text"
-            name="weapon-notes"
-            id={`weapon-notes-${name}`}
-            value={wepNotes}
-          />
-        </div>
+      <div className="col-span-6 flex flex-col justify-between">
+        <label className="input-label" htmlFor="weapon-notes">NOTES</label>
+
+        <input
+          className="small-input input-string-text "
+          type="text"
+          name="weapon-notes"
+          id={`weapon-notes-${name}`}
+          value={wepNotes}
+          onChange={(e) => setWepNotes(e.target.value)}
+        />
       </div>
-      <div className="row-start-3 col-span-8">
-        <div className="flex flex-row justify-center gap-3">
-          <div>
-            <label htmlFor="weapon-ammo">Ammunition</label>
-            <div>
-              <input
-                className="tiny-input"
-                type="text"
-                name="weapon-ammo"
-                id={`weapon-ammo-${name}`}
-                value={wepAmmo}
-              />
-            </div>
+      <div className="col-span-3">
+        <div className="flex flex-row gap-2">
+          <div className="w-1/3 md:w-1/2">
+            <label className="input-label" htmlFor="weapon-ammo">
+              Ammunition
+            </label>
+
+            <input
+              className="small-input input-string-text "
+              type="text"
+              name="weapon-ammo"
+              id={`weapon-ammo-${name}`}
+              value={wepAmmo}
+              onChange={(e) => setWepAmmo(e.target.value)}
+            />
           </div>
-          <div>
-            <label htmlFor="weapon-ammo-max">Max</label>
-            <div>
-              <input
-                className="tiny-input"
-                type="number"
-                name="weapon-ammo-max"
-                id={`weapon-ammo-max-${name}`}
-                value={wepAmmoMax}
-              />
-            </div>
+          <span className="self-end text-3xl md:text-4xl">/</span>
+          <div className="w-1/3 md:w-1/2">
+            <label className="input-label" htmlFor="weapon-ammo-max">Max</label>
+
+            <input
+              className="small-input input-string-text "
+              type="number"
+              name="weapon-ammo-max"
+              id={`weapon-ammo-max-${name}`}
+              value={wepAmmoMax}
+              onChange={(e) => setWepAmmoMax(e.target.value)}
+            />
           </div>
         </div>
       </div>
