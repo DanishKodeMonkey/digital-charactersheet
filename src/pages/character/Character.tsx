38cc76aeca854975c367b1359c47b356de01c1f6
danishkodemonkey <humberjini@hotmail.com>
2024-12-09 14:16:12 +0100

    hoist state management of feats and special abilities to parent component

--- src/pages/character/Character.tsx
+++ src/pages/character/Character.tsx
@@ -1,11 +1,31 @@
-import Feats from "../../components/character/Miscellaneous/Feats.tsx";
+import {useState} from 'react'
+
+import Items from "../../components/character/Miscellaneous/Items.tsx";
 
 function Character() {
+  const[feats, setFeats] = useState([])
+  const [specialAbilities, setSpecialAbilities ] = useState([])
+
+  const addFeat = (feat) => setFeats((prev) => [...prev, feat])
+  const removeFeat = (index) => setFeats((prev) => prev.filter((item) => item.index !== index))
+  
+  const addSpecialAbility = (SpecialAbility) => setSpecialAbilities((prev) => [...prev, SpecialAbility])
+  const removeSpecialAbility = (index) => setSpecialAbilities((prev) => prev.filter((item) => item.index !== index))
+  
   return (
     <div>
       <h1>Character</h1>
       <div>
-        <Feats />
+      <Items itemType="Feats"
+      items={feats}
+      addItem={addFeat}
+      removeItem={removeFeat} />
+      </div>
+      <div>
+      <Items itemType="Special Abilities"
+      items={specialAbilities}
+      addItem={addSpecialAbility}
+      removeItem={removeSpecialAbility} />
       </div>
     </div>
   );
