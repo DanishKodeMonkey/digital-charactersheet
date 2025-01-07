1b6c1b9079b73361ef871c3c419300abe9917070
danishkodemonkey <humberjini@hotmail.com>
2025-01-07 15:48:02 +0100

    First pass of centralization context for wrapper in application

---
+++ src/components/CentralisationLayer/CentralisationContext.tsx
@@ -0,0 +1,38 @@
+import React, {createContext, useContext, useReducer, ReactNode} from 'react'
+import { centralizationReducer, centralState, State, Action } from "./CentralisationLayer.ts"
+
+// Type definitions, HUSKAAT: Move to seperate file
+
+interface CentralizationContextProps{
+    state: State;
+    dispatch: React.Dispatch<Action>;
+}
+
+// Define provider props
+interface CentralizationProviderProps{
+    children: ReactNode
+}
+
+// establish context
+const CentralizationContext = createContext<CentralizationContextProps | undefined>(undefined)
+
+// Establish provider
+
+export const CentralizationProvider: React.FC<CentralizationProviderProps> = ({children}) =>{
+    const [state, dispatch] = useReducer(centralizationReducer, centralState)
+
+    return(
+        <CentralizationContext.Provider value={{state, dispatch}}>
+            {children}
+        </CentralizationContext.Provider>
+    )
+}
+
+// Custom hook to use context
+export const useCentralization = (): CentralizationContextProps =>{
+    const context = useContext(CentralizationContext)
+    if(!context){
+        throw new Error('useCentralization must be used within a centralisationProvider')
+    }
+    return context;
+}
