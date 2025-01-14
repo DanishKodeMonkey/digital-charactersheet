2aa07bc7e951d3265057d6935c791c3cc6586a54
danishkodemonkey <humberjini@hotmail.com>
2025-01-14 14:43:33 +0100

    Refactor to properly utilise centralisationLayer and call dispatches as needed further refactor to add new field for healing/damage and current health HUSKAT: Update to calculate max health automatically

--- src/components/character/Status/Health.tsx
+++ src/components/character/Status/Health.tsx
@@ -1,58 +1,85 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import { useCentralization } from "../../CentralisationLayer/CentralisationContext.tsx";
+import {HealthStatus} from "../../CentralisationLayer/CentralisationLayer.ts"
 
 
 function Health() {
-  const [healthPoints, setHealthPoints] = useState<number>(30);
-  const [hitDie, setHitDie] = useState<number>("");
-  const constitutionModifier = 2;
+  const {state, dispatch} = useCentralization();
+  const {maxHealth, currentHealth, damage} = state.health
 
-  const updateHealth = () => {
-    const newHealth = hitDie + constitutionModifier;
-    setHealthPoints((prevHealth) => prevHealth + newHealth);
-  };
+  const handleChange = (stat: keyof HealthStatus, value: number) =>{
+    if(!isNaN(value)){
+    dispatch({
+      field: 'health',
+      type: "UPDATE_HEALTH_FIELD",
+      payload: {stat: stat, value}
+    })}
+  }
 
-  const handleSubmit = () => {
-    updateHealth();
-    setHitDie("");
-  };
+  const handleDamageOrHeal = () =>{
+    if(damage < 0){
+    dispatch({
+      field: 'health', type: "HEALTH_DAMAGE",
+      payload: {value: damage}
+    })}else if (damage > 0){
+      dispatch({
+        field: 'health',
+        type: "HEALTH_HEAL",
+        payload: {value: damage}
+      })
+    }
 
+  }
+  
+
+  
   return (
-    <div className="grid grid-rows-2">
-      <div className="grid grid-cols-4 text-center">
+    <div className="grid grid-rows-2 my-2 items-center">
+      <div className="grid grid-cols-4 text-center items-center">
         <label htmlFor="HP" className="input-label col-span-1">
-          Health <br /> Points
+          Max <br /> Health
         </label>
-        <span className="col-span-1 text-xs md:input-label">+</span>
+        <label htmlFor="currentHealth" className="input-label col-span-1">
+          current <br /> Health
+        </label>
         <label htmlFor="hitDie" className="input-label col-span-1">
-          Hit die
+          Damage/Healing
         </label>
       </div>
       <div className="grid grid-cols-4 gap-3 text-center">
-        <input
-          type="number"
-          className="input-small w-full col-span-1"
-          name="HP"
-          id="HP"
-          disabled
-          value={healthPoints}
-        />
-        <span className="col-span-1 text-xs md:input-label">+</span>
-        <input
-          className="input-small w-full col-span-1"
-          type="number"
-          name="hitDie"
-          id="hitDie"
-          value={hitDie}
-          onChange={(e) => setHitDie(Number(e.target.value))}
-        />
-        <button
-          type="submit"
-          className="col-span-1 btn btn-primary"
-          onClick={handleSubmit}
-        >
-          Add
+        <div>
+          <input
+            type="number"
+            className="input-small w-full col-span-1"
+            name="maxHP"
+            id="maxHP"
+            readOnly
+            value={maxHealth}
+          />
+        </div>
+        <div>
+          <input type="number"           className="input-small w-full col-span-1"
+           name="currentHealth" id="currentHealth" value={currentHealth} onChange={(e) => handleChange('currentHealth', Number(e.target.value))}/>
+
+        </div>
+        <div>          <input
+            className="input-small w-full col-span-1"
+            type="number"
+            name="currentHealth"
+            id="currentHealth"
+            value={damage}
+            onChange={(e) => handleChange('damage', Number(e.target.value))}
+          /></div>
+        <div>
+          <button
+            type="submit"
+            className="col-span-1 btn btn-primary"
+            onClick={handleDamageOrHeal}
+          >
+
+          Apply 
         </button>
+        </div>
       </div>
     </div>
   );
