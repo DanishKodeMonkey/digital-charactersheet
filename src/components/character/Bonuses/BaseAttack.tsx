ddab027288fcb2aa4a54277b41ee2a3435fda4cb
danishkodemonkey <humberjini@hotmail.com>
2025-01-27 14:34:53 +0100

    Implement central state calculated base attack bonus

--- src/components/character/Bonuses/BaseAttack.tsx
+++ src/components/character/Bonuses/BaseAttack.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import { useCentralization } from "../../CentralisationLayer/CentralisationContext.tsx";
 
 function BaseAttackBonus() {
+  const { state, dispatch } = useCentralization();
   /*
      Your attack bonus with a melee weapon is:
 
+
+     HUSKAT
+     base attack is determined by class, update to pull from characterDetails.class. once updated
     Base attack bonus + Strength modifier + size modifier
 
     With a ranged weapon, your attack bonus is:
@@ -11,25 +16,68 @@ function BaseAttackBonus() {
     Base attack bonus + Dexterity modifier + size modifier + range penalty
 
     */
+  const baseAttackBonus = 1; // TEMP
+  const strengthMod = state.stats.modifiers.strength;
+  const sizeMod = state.characterDetails.size.ACMod;
 
-  const [baseAtk, setBaseAtk] = useState<number>(5);
-  const handleBaseAtkChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(e.target.value, 10);
-    setBaseAtk(value);
-  };
+  const baseAtk = baseAttackBonus + strengthMod + sizeMod;
+
   return (
-    <div className="flex flex-col m-5">
+    <div className="flex flex-col m-5 text-center">
       <label htmlFor="BaseAttackBonus" className="input-title">
         Base Attack Bonus
       </label>
-      <input
-        className="input-base w-full"
-        type="number"
-        name="baseAtkBonus"
-        id="baseAtkBonus"
-        value={baseAtk}
-        onChange={handleBaseAtkChange}
-      />
+      <div className="input-container">
+        <div className="input-container-col">
+          <input
+            className="input-base w-full"
+            type="number"
+            name="baseAtkBonus"
+            id="baseAtkBonus"
+            value={baseAtk}
+            readOnly
+          />
+          <label htmlFor="baseAtkBonus">Total</label>
+        </div>
+        <span className="mt-4">=</span>
+
+        <div className="input-container-col">
+          <input
+            className="input-base w-full"
+            type="number"
+            name="bonusMod"
+            id="bonusMod"
+            value={baseAttackBonus}
+            readOnly
+          />
+          <label htmlFor="bonusMod">Base attack</label>
+        </div>
+        <span className="mt-4">+</span>
+        <div className="input-container-col">
+          <input
+            className="input-base w-full"
+            type="number"
+            name="strengthMod"
+            id="strengthMod"
+            value={strengthMod}
+            readOnly
+          />
+          <label htmlFor="strengthMod">Strength mod</label>
+        </div>
+        <span className="mt-4">+</span>
+        <div className="input-container-col">
+          <input
+            className="input-base w-full"
+            type="number"
+            name="sizeMod"
+            id="sizeMod"
+            value={sizeMod}
+            readOnly
+          />
+          <label htmlFor="sizeMod">Size mod</label>
+        </div>
+
+      </div>
     </div>
   );
 }
