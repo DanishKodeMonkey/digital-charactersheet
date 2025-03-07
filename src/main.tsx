04b0daa3851723d04775b997ba7baf8862180253
danishkodemonkey <humberjini@hotmail.com>
2025-03-07 14:49:20 +0100

    Expand entrypoint to determine initial routing based on authentication

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './App.tsx'
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
+import Home from "./pages/Home.tsx";
+import { AuthRoutes, AppRoutes, UserRoutes } from "./routes/index.tsx";
+
+
+/* Entrypoint determines direction of user. */
+
+const Main = () =>{
+
+  return (
+    <Router>
+      <Routes>
+        {/* Landing page, determines where to send user */}
+        <Route path="/" element={<Home />} />
+
+        {/* Authentication page, Login and signup */}
+        <Route path="/auth/*" element={<AuthRoutes />} />
+
+        {/* App layer, the actual character sheet app */}
+        <Route path="/app/" element={<AppRoutes /> } />
+
+        {/* User Settings / Profile management */}
+        <Route path="/user/" element={<UserRoutes />} />
+
+        {/* Final catch-all, re-route to home */}
+        <Route path="*" element={<Navigate to="/" />} />
+
+      </Routes>
+    </Router>
+  )
+
+}
+
+
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
 );
