873e15c6715a5d032507e1b6f4ae74d56beab1d5
danishkodemonkey <humberjini@hotmail.com>
2025-03-11 14:39:43 +0100

    Add error handling layer

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,36 +9,62 @@ if (!API_URL) {
     throw new Error('Please define API url in environment variables');
 }
 
+// Helper function for API errors
+const handleAPIError = (error: any) => {
+    if (axios.isAxiosError(error)) {
+        // If axios error, return response message if any
+        const status = error.response?.status || 'Unknown';
+        const message =
+            error.response?.data?.message ||
+            error.message ||
+            'An unknown error has occurred';
+        throw new Error(`API Error (${status}): ${message}`);
+    } else {
+        // Handle non-Axios errors (unexpected issue has occured)
+        throw new Error('Unexpected error has occured');
+    }
+};
+
 // Auth API function object
 export const auth = {
     signIn: async (email: string, password: string) => {
-        const response = await axios.post(`${API_URL}/auth/signin`, {
-            email,
-            password,
-        });
-        if (response.status != 200) {
-            throw new Error(
-                `An error occured while signing in ${response.status} - ${response.statusText}`
-            );
-        } else {
+        try {
+            const response = await axios.post(`${API_URL}/auth/signin`, {
+                email,
+                password,
+            });
             return response.data;
+        } catch (error) {
+            handleAPIError(error);
         }
     },
     signUp: async (username: string, email: string, password: string) => {
-        const response = await axios.post(`${API_URL}/auth/signup`, {
-            username,
-            email,
-            password,
-        });
-        return response.data;
+        try {
+            const response = await axios.post(`${API_URL}/auth/signup`, {
+                username,
+                email,
+                password,
+            });
+            return response.data;
+        } catch (error) {
+            handleAPIError(error);
+        }
     },
     refreshToken: async (refreshToken: string) => {
-        const response = await axios.post(`${API_URL}/auth/refresh`, {
-            token: refreshToken,
-        });
-        return response.data;
+        try {
+            const response = await axios.post(`${API_URL}/auth/refresh`, {
+                token: refreshToken,
+            });
+            return response.data;
+        } catch (error) {
+            handleAPIError(error);
+        }
     },
     signout: async () => {
-        await axios.post(`${API_URL}/auth/signout`);
+        try {
+            await axios.post(`${API_URL}/auth/signout`);
+        } catch (error) {
+            handleAPIError(error);
+        }
     },
 };
