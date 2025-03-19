9afb40d48e7dbb1420c6b364d7bcb9fd8f3f2fa0
danishkodemonkey <humberjini@hotmail.com>
2025-03-19 15:22:51 +0100

    Properly handle JWT auth from API instead of local

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,24 +1,12 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import SignInPage from "../pages/auth/SignIn.tsx";
-import SignUpPage from "../pages/auth/SignUp.tsx";
-import { useAuth } from "../context/authentication/AuthContext.tsx";
+import {Outlet} from "react-router-dom"
 
 const AuthRoutes = () => {
-  const isAuthenticated = useAuth();
 
   return (
-    <Routes>
-      <Route
-        path="/signin"
-        element={isAuthenticated ? <Navigate to="/app" /> : <SignInPage />}
-      />
-      <Route
-        path="/signup"
-        element={isAuthenticated ? <Navigate to="/app" /> : <SignUpPage />}
-      />
-      <Route path="*" element={<Navigate to="/signin" />} />{" "}
-      /* Defaults to sign-in */
-    </Routes>
+    <div>
+      <h1>Authentication</h1>
+      <Outlet />
+    </div>
   );
 };
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,69 +1,100 @@
 // API interaction layer.
 
-import axios from "axios";
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 // Define API base URL
 const API_URL = import.meta.env.VITE_API_URL;
 
 if (!API_URL) {
-  throw new Error("Please define API url in environment variables");
+    throw new Error('Please define API url in environment variables');
 }
 
 // Helper function for API errors
 const handleAPIError = (error: any) => {
-  if (axios.isAxiosError(error)) {
-    // If axios error, return response message if any
-    const status = error.response?.status || "Unknown";
-    const message = error.response?.data?.message ||
-      error.message ||
-      "An unknown error has occurred";
-    throw new Error(`API Error (${status}): ${message}`);
-  } else {
-    // Handle non-Axios errors (unexpected issue has occured)
-    throw new Error("Unexpected error has occured");
-  }
+    if (axios.isAxiosError(error)) {
+        // If axios error, return response message if any
+        const status = error.response?.status || 'Unknown';
+        const message =
+            error.response?.data?.message ||
+            error.message ||
+            'An unknown error has occurred';
+        throw new Error(`API Error (${status}): ${message}`);
+    } else {
+        // Handle non-Axios errors (unexpected issue has occured)
+        throw new Error('Unexpected error has occured');
+    }
 };
 
 // Auth API function object
 export const auth = {
-  signIn: async (email: string, password: string) => {
-    try {
-      const response = await axios.post(`${API_URL}/auth/signin`, {
-        email,
-        password,
-      });
-      return response.data;
-    } catch (error) {
-      handleAPIError(error);
-    }
-  },
-  signUp: async (username: string, email: string, password: string) => {
-    try {
-      const response = await axios.post(`${API_URL}/auth/signup`, {
-        username,
-        email,
-        password,
-      });
-      return response.data;
-    } catch (error) {
-      handleAPIError(error);
-    }
-  },
-  refreshToken: async (refreshToken: string) => {
-    try {
-      const response = await axios.post(`${API_URL}/auth/refresh`, {
-        token: refreshToken,
-      });
-      return response.data;
-    } catch (error) {
-      handleAPIError(error);
-    }
-  },
-  signout: async () => {
-    try {
-      await axios.post(`${API_URL}/auth/signout`);
-    } catch (error) {
-      handleAPIError(error);
-    }
-  },
+    signIn: async (
+        email: string,
+        password: string,
+        login: (access_token: string, username: string) => void
+    ) => {
+        try {
+            const response = await axios.post(`${API_URL}/auth/signin`, {
+                email,
+                password,
+            });
+            console.log(response);
+
+            const { access_token, username } = response.data;
+            login(access_token, username);
+
+            return response.data;
+        } catch (error) {
+            handleAPIError(error);
+            return null;
+        }
+    },
+    signUp: async (username: string, email: string, password: string) => {
+        try {
+            const response = await axios.post(`${API_URL}/users/signup`, {
+                username,
+                email,
+                password,
+            });
+            return response.data;
+        } catch (error) {
+            handleAPIError(error);
+        }
+    },
+    refreshToken: async () => {
+        const refreshToken = Cookies.get('refresh_token');
+        if (!refreshToken) {
+            throw new Error('No refresh token available');
+        }
+        try {
+            const response = await axios.post(`${API_URL}/auth/refresh`, {
+                refresh_token: refreshToken,
+            });
+            return response.data;
+        } catch (error) {
+            handleAPIError(error);
+        }
+    },
+    verifyToken: async (accessToken: string, refreshToken: string) => {
+        try {
+            const response = await axios.post(`${API_URL}/auth/verify`, {
+                accessToken,
+                refreshToken,
+            });
+            const { access_token } = response.data;
+            if (access_token) {
+                sessionStorage.setItem('access_token', access_token);
+            }
+        } catch (error) {
+            handleAPIError(error);
+            return false;
+        }
+    },
+    signout: async () => {
+        try {
+            await axios.post(`${API_URL}/auth/signout`);
+        } catch (error) {
+            handleAPIError(error);
+        }
+    },
 };
