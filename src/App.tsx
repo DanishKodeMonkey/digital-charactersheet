3a1bd035bdac2b9ac250a8330d61ae48731bad30
danishkodemonkey <humberjini@hotmail.com>
2025-03-24 13:11:27 +0100

    Remove routing to App.tsx and simplify main.tsx to mount to root with auth context (seperation of concerns)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,52 @@
 /// <reference path="./images.d.ts" />
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header.tsx";
 
+
+import AppRoutes from "./routes/AppRoutes.tsx";
 import Overview from "./pages/character/Overview.tsx";
 import Character from "./pages/character/Character.tsx";
 import Inventory from "./pages/character/Inventory.tsx";
 import Spells from "./pages/character/Spells.tsx";
 
-import { CentralizationProvider } from "./context/CentralisationLayer/CentralisationContext.tsx";
+import Home from "./pages/Home.tsx";
+
+import AuthRoutes from "./routes/AuthRoutes.tsx";
+import SignIn from "./pages/auth/SignIn.tsx";
+import SignUp from "./pages/auth/SignUp.tsx";
+
+import UserRoutes from "./routes/UserRoutes.tsx";
+
 
 function App() {
   return (
-    <CentralizationProvider>
-      <div className="App">
-        <Router>
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<Overview />} />
-              <Route path="/inventory" element={<Inventory />} />
-              <Route path="/character" element={<Character />} />
-              <Route path="/spells" element={<Spells />} />
-            </Routes>
-          </main>
-        </Router>
-      </div>
-    </CentralizationProvider>
+    <Router>
+      <Routes>
+        {/* Landing page, determines where to send user */}
+        <Route path="/" element={<Home />} />
+
+        {/* Authentication page, Login and signup */}
+        <Route path="auth" element={<AuthRoutes />}>
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+        </Route>
+
+        {/* App layer, the actual character sheet app */}
+        <Route path="app" element={<AppRoutes />}>
+              <Route index element={<Overview />} />
+              <Route path="inventory" element={<Inventory />} />
+              <Route path="character" element={<Character />} />
+              <Route path="spells" element={<Spells />} />
+        </Route>
+
+        {/* User Settings / Profile management */}
+        <Route path="/user/" element={<UserRoutes />} />
+
+        {/* Final catch-all, re-route to home */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,59 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
-import Home from "./pages/Home.tsx";
-import { AppRoutes, AuthRoutes, UserRoutes } from "./routes/index.tsx";
-import { AuthProvider } from "./context/authentication/AuthContext.tsx";
-import SignIn from "./pages/auth/SignIn.tsx";
-import SignUp from "./pages/auth/SignUp.tsx";
-import Overview from "./pages/character/Overview.tsx";
-import Inventory from "./pages/character/Inventory.tsx";
-import Character from "./pages/character/Character.tsx";
-import Spells from "./pages/character/Spells.tsx";
 import App from "./App.tsx";
-
-/* Entrypoint determines direction of user. */
-
-const Main = () => {
-  return (
-    <Router>
-      <Routes>
-        {/* Landing page, determines where to send user */}
-        <Route path="/" element={<Home />} />
-
-        {/* Authentication page, Login and signup */}
-        <Route path="auth" element={<AuthRoutes />}>
-          <Route path="signin" element={<SignIn />} />
-          <Route path="signup" element={<SignUp />} />
-        </Route>
-
-        {/* App layer, the actual character sheet app */}
-        <Route path="app" element={<AppRoutes />}>
-              <Route index element={<Overview />} />
-              <Route path="inventory" element={<Inventory />} />
-              <Route path="character" element={<Character />} />
-              <Route path="spells" element={<Spells />} />
-        </Route>
-
-        {/* User Settings / Profile management */}
-        <Route path="/user/" element={<UserRoutes />} />
-
-        {/* Final catch-all, re-route to home */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
-  );
-};
+import { AuthProvider } from "./context/authentication/AuthContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
-      <Main />
+      <App />
     </AuthProvider>
   </React.StrictMode>,
 );
